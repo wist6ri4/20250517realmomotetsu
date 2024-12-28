d5214772de2bc15067fe6caf72054997be939269
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2024-12-28 21:45:45 +0900

    refactor: :recycle: 20241228 定数のリファクタリング/チームごとのメソッドを共通化

--- js/constantsForIndex.js
+++ js/constantsForIndex.js
@@ -7,8 +7,14 @@ const CFI = {
     DIGITAL_DISPLAY_JP: $('#digital-display-jp'), // 電光掲示板
     DESTINATION_STATION: $('#destination-station'), // 電光掲示板
     UPDATED_TIME: $('#updated-time'), // 更新時刻表示部
+    INVISIBLE_TRAIN: 'invisible-train', // 電車非表示クラス
+}
+
+const TEAMS = {
     TEAM_A: {
+        IS_ADDED: 'teamAIsAdded', // 追加されたかどうか
         TEAM_NAME: Constants.TEAM_A_NAME, // チーム名
+        TEAM_KEY: 'teamA', // チームキー
         TRAIN: $('#team-a-train'), // 電車コマ
         TRAIN_VISIBILITY: $('#team-a-train-visibility'), // 電車コマの表示・非表示
         INFORMATION_NAME: $('#team-a-information-name'), // チーム情報表示部のチーム名
@@ -19,7 +25,9 @@ const CFI = {
         MODAL_NAME: $('#team-a-modal-name'), // チーム情報モーダルのチーム名
     },
     TEAM_B: {
+        IS_ADDED: 'teamBIsAdded', // 追加されたかどうか
         TEAM_NAME: Constants.TEAM_B_NAME, // チーム名
+        TEAM_KEY: 'teamB', // チームキー
         TRAIN: $('#team-b-train'), // 電車コマ
         TRAIN_VISIBILITY: $('#team-b-train-visibility'), // 電車コマの表示・非表示
         INFORMATION_NAME: $('#team-b-information-name'), // チーム情報表示部のチーム名
@@ -30,7 +38,9 @@ const CFI = {
         MODAL_NAME: $('#team-b-modal-name'), // チーム情報モーダルのチーム名
     },
     TEAM_C: {
+        IS_ADDED: 'teamCIsAdded', // 追加されたかどうか
         TEAM_NAME: Constants.TEAM_C_NAME, // チーム名
+        TEAM_KEY: 'teamC', // チームキー
         TRAIN: $('#team-c-train'), // 電車コマ
         TRAIN_VISIBILITY: $('#team-c-train-visibility'), // 電車コマの表示・非表示
         INFORMATION_NAME: $('#team-c-information-name'), // チーム情報表示部のチーム名
@@ -41,7 +51,9 @@ const CFI = {
         MODAL_NAME: $('#team-c-modal-name'), // チーム情報モーダルのチーム名
     },
     TEAM_D: {
+        IS_ADDED: 'teamDIsAdded', // 追加されたかどうか
         TEAM_NAME: Constants.TEAM_D_NAME, // チーム名
+        TEAM_KEY: 'teamD', // チームキー
         TRAIN: $('#team-d-train'), // 電車コマ
         TRAIN_VISIBILITY: $('#team-d-train-visibility'), // 電車コマの表示・非表示
         INFORMATION_NAME: $('#team-d-information-name'), // チーム情報表示部のチーム名
@@ -55,3 +67,6 @@ const CFI = {
 
 Object.freeze(CFI);
 export { CFI };
+
+Object.freeze(TEAMS);
+export { TEAMS };
