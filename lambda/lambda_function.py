9e6f53dba50f0ab7c0246502bf98228469d5ed6a
Shiro Goto <sg1106.footballdrummer.51046@gmail.com>
2025-03-05 18:15:50 +0900

    feature: :sparkles: 20250305 Discord通知用Lambdaの管理

---
+++ lambda/lambda_function.py
@@ -0,0 +1,76 @@
+import json
+import os
+import textwrap
+import logging
+from datetime import datetime
+
+import requests
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    """
+    lambda_handler receives a POST request from Supabase Webhook.
+    And then sends a message to Discord Webhook.
+    """
+    try:
+        logger.info('Start AWS Lambda')
+
+        # SupabaseのWebhookからのデータを取得
+        record = (json.loads(event['body'])).get('record')
+        logger.info('Received record: %s', record)
+        if not record:
+            logger.error('No record found in the event body')
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Invalid request'})
+            }
+
+        team_id = record['team_id']
+        station_id = record['station_id']
+        created_at = record['created_at']
+
+        if not all([team_id, station_id, created_at]):
+            logger.error('Missing required fields: %s', record)
+            return {
+                'statusCode': 400,
+                'body': json.dumps({'message': 'Missing required fields'})
+            }
+
+        # Discordのメッセージフォーマットに変換
+        discord_message = {
+            "content": textwrap.dedent(
+                f"""
+                現在地情報が登録されました：
+                > チームID: {record['team_id']}
+                > 駅ID: {record['station_id']}
+                > 登録日時: {datetime.fromisoformat(created_at).strftime('%Y/%m/%d %H:%M:%S')}
+                """)
+        }
+
+        # Discord Webhookに送信
+        response = requests.post(os.getenv('DISCORD_WEBHOOK_URL'), json=discord_message, timeout=10)
+
+        if response.status_code == 204:
+            logger.info('Message sent to Discord successfully')
+        else:
+            logger.error(
+                'Failed to send message to Discord. Status code: %s - %s',
+                response.status_code,
+                response.text
+            )
+
+        logger.info('Discord message: %s', discord_message)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Message sent to Discord successfully'})
+        }
+
+    except Exception as e:
+        logger.error('An error occurred: %s', e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Internal server error'})
+        }
